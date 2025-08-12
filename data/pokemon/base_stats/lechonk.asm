2d39109539aaafb2989c1b93278494da6dc83efc
Larson T. (Lrs121) <ltthorpe@lrsservers.org>
2025-08-12 13:22:30 -0700

    update pic make

--- data/pokemon/base_stats/lechonk.asm
+++ data/pokemon/base_stats/lechonk.asm
@@ -8,13 +8,13 @@
 	db 211 ; base exp
 
 	INCBIN "gfx/pokemon/front/lechonk.pic", 0, 1 ; sprite dimensions
-	dw LechonkF, LechonkB
+	dw LechonkFrontPic, LechonkBackPic
 
 	db TACKLE, TAIL_WHIP, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  BUBBLE_BEAM,  \
+	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,  \
 	     DIG,          BIDE,         REST,         SUBSTITUTE                  \
 	; end
 
